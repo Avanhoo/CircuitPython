7f8007e16f76262486625b6118591d6f5525370f
Avanhoo <avanhoo2498@charlottesvillecityschools.org>
2022-09-12 09:06:01 -0400

    Added control of R, B, & B values

--- code.py
+++ code.py
@@ -1,7 +1,20 @@
+import board
+import neopixel
 from time import sleep
 
+r = 0
+g = 0
+b = 0
+
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+dot.brightness = .25
+
+print("Make it red!")
+
 while True:
-    print("waffle")
+    r = int(input("How much red?"))
+    g = int(input("How much green?"))
+    b = int(input("How much blue?"))
     sleep(1)
-    if True:
-        input("and bolts")
+    print("")
+    dot.fill((r,g,b))
