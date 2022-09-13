1ad3df7a4fe2a9708d7fa25123b1a05bc14df95e
Avanhoo <avanhoo2498@charlottesvillecityschools.org>
2022-09-13 09:44:48 -0400

    Added servo.py, controlls 180 servo and needed lib

---
+++ servo.py
@@ -0,0 +1,30 @@
+import board
+import neopixel
+from time import sleep
+import pwmio
+import servo
+from digitalio import DigitalInOut, Direction, Pull
+angle = 90
+
+
+pwm = pwmio.PWMOut(board.A1, duty_cycle=2 ** 15, frequency=50)
+
+# Create a servo object, my_servo.
+my_servo = servo.Servo(pwm)
+
+button = DigitalInOut(board.D7)
+button.direction = Direction.INPUT
+button2 = DigitalInOut(board.D6)
+button2.direction = Direction.INPUT
+
+
+while True:
+    if button.value and angle < 180:
+        angle += 1
+
+    if button2.value and angle > 0:
+        angle -=1
+    
+    print(angle)
+    my_servo.angle = angle
+    sleep(0.01)
