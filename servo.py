8d9b69a8b665892d1b6e1d44bffb657d16b88bda
Avanhoo <avanhoo2498@charlottesvillecityschools.org>
2023-02-21 08:59:06 -0500

    Small changes to servo control

--- servo.py
+++ servo.py
@@ -5,10 +5,13 @@ from time import sleep
 import pwmio
 import servo
 from digitalio import DigitalInOut, Direction
-angle = 90
+from analogio import AnalogIn
+import simpleio
+angle = 0
 
 
 pwm = pwmio.PWMOut(board.A1, duty_cycle=2 ** 15, frequency=50)
+pot = AnalogIn(board.A2)
 
 # Create a servo object, my_servo.
 my_servo = servo.Servo(pwm)
@@ -20,12 +23,12 @@ button2.direction = Direction.INPUT
 
 
 while True:
-    if button.value and angle < 180:
-        angle += 1
-
-    if button2.value and angle > 0:
-        angle -=1
-    
+    angle = round(simpleio.map_range(pot.value, 2550, 65520, 0, 180))
     print(angle)
-    my_servo.angle = angle
-    sleep(0.01)
+    if angle>90:
+        for i in range(4,175):
+            my_servo.angle = i
+        for i in range (175,4,-1):
+            my_servo.angle = i
+    my_servo.angle = 90
+    sleep(1)
