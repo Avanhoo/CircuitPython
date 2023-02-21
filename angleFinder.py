231f216db10b2180a3c34fe7b5d02e9ab90b38df
Avanhoo <avanhoo2498@charlottesvillecityschools.org>
2023-02-21 08:57:26 -0500

    Created tic-tac-toe code and control

---
+++ angleFinder.py
@@ -0,0 +1,18 @@
+
+import board
+from time import sleep
+import pwmio
+import servo
+
+offset = 3
+
+num = "90"
+pwm = pwmio.PWMOut(board.A1, duty_cycle=2 ** 15, frequency=50)
+pwm2 = pwmio.PWMOut(board.A5, duty_cycle=2 ** 15, frequency=50)
+
+arm = servo.Servo(pwm)
+spinny = servo.Servo(pwm2)
+spinny.angle = 90 + offset
+while True:
+    spinny.angle = int(input("A: ")) + offset
+    arm.angle = int(input("D: "))
---
+++ tictactod.py
@@ -0,0 +1,354 @@
+import board
+from time import sleep
+import random
+import pwmio
+import servo
+import math
+
+turn = 0 # whose turn it is, 0 is player, 1 is AI
+round = 1 # which round of the game it is
+end = 0 # if the game has ended
+plan = 0 # where the AI is planning to move
+extend = 0 # how far out the arm is
+twist = 0 # angle of turn of the arm
+offset = 3
+armProg = 96
+
+pwm = pwmio.PWMOut(board.A1, duty_cycle=2 ** 15, frequency=50)
+pwm2 = pwmio.PWMOut(board.A5, duty_cycle=2 ** 15, frequency=50)
+#pwm3 = pwmio.PWMOut(board.A4, duty_cycle=2 ** 15, frequency=50)
+theBoard = {'7': ' ' , '8': ' ' , '9': ' ' ,
+            '4': ' ' , '5': ' ' , '6': ' ' ,
+            '1': ' ' , '2': ' ' , '3': ' ' }
+
+distBoard = {'7': 170 , '8': 157 , '9': 170 ,
+            '4': 110 , '5': 96 , '6': 110 ,
+            '1': 58 , '2': 30 , '3': 61 }
+angleBoard = {'7': 110 , '8': 90 , '9': 70 ,
+            '4':  121, '5': 90 , '6': 63 ,
+            '1': 142 , '2': 90 , '3': 43 }
+arm = servo.Servo(pwm)
+spinny = servo.Servo(pwm2)
+#angleServo = servo.Servo(pwm2)
+#                                                  ARM SERVO SHOULD HAVE ONE TOOTH SHOWING IN THE BACK WHEN AT '5' POSITION
+
+def printBoard(board):
+    print("")
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
+    print('-+-+-')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print('-+-+-')
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+
+def checkWin():
+            global end
+            if theBoard['7'] == theBoard['8'] == theBoard['9'] and theBoard['9'] != ' ': # across the top
+                end += 1      
+                print("Across Top")     
+            
+            elif theBoard['4'] == theBoard['5'] == theBoard['6'] and theBoard['6'] != ' ': # across the middle
+                end += 1
+                print("Across Mid")       
+            
+            elif theBoard['1'] == theBoard['2'] == theBoard['3'] and theBoard['3'] != ' ': # across the bottom
+                end += 1
+                print("Across Bottom")
+                
+            elif theBoard['1'] == theBoard['4'] == theBoard['7'] and theBoard['7'] != ' ': # down the left side
+                end += 1
+                print("Down Left")
+                
+            elif theBoard['2'] == theBoard['5'] == theBoard['8'] and theBoard['8'] != ' ': # down the middle
+                end += 1
+                print("Down Mid")
+                
+            elif theBoard['3'] == theBoard['6'] == theBoard['9'] and theBoard['9'] != ' ': # down the right side
+                end += 1
+                print("Down Right")
+                 
+            elif theBoard['7'] == theBoard['5'] == theBoard['3'] and theBoard['3'] != ' ': # diagonal
+                end += 1
+                print("Diagonal 7-3")
+                
+            elif theBoard['1'] == theBoard['5'] == theBoard['9'] and theBoard['9'] != ' ': # diagonal
+                end += 1
+                print("Diagonal 9-1")
+                 
+
+def place(spot):
+    armProg = arm.angle
+    sleep(1)
+    theBoard[str(spot)] = "O"
+    spinny.angle = (angleBoard[str(spot)] + offset)
+    sleep(.25)
+    while arm.angle != distBoard[str(spot)]:
+        if abs(armProg - distBoard[str(spot)]) < 2:
+            arm.angle = (distBoard[str(spot)] * 0.86925636203 + 5)
+            break
+        elif armProg < distBoard[str(spot)]:
+            armProg += 1
+        elif armProg > distBoard[str(spot)]:
+            armProg -= 1
+        arm.angle = (armProg * 0.86925636203 + 5) 
+        sleep(.001)
+
+    print(arm.angle)
+    #angleServo.angle(angleBoard[str(spot)])
+
+arm.angle = distBoard['5']
+spinny.angle = 90 + offset
+print("Move with the numpad.")
+sleep(1)
+
+
+for i in range(5):
+    plan = 0
+    print("Round " + str(round) + ", Your Turn")
+    turn = 0
+    printBoard(theBoard)
+    print("Where Would you like to move?")
+    move = input("")
+
+    try:
+        if int(move) < 10 and  int(move) > 0 and theBoard[move] == " ":
+            theBoard[str(move)] = "X"
+            round += 1
+        else:
+            print("Invalid move, try again")
+            continue
+    except:
+        print("Invalid move, try again")
+        continue
+
+    checkWin()
+    if end == 1:
+        i = 6
+        break
+    elif round == 10:
+        turn = 2
+        break
+
+    printBoard(theBoard)
+    sleep(1)
+    turn = 1
+    if True:
+        if theBoard['7'] == theBoard['8'] == "O" and theBoard['9'] == ' ': # across the top
+            place(9)
+            round += 1
+            turn += 1
+        elif theBoard['9'] == theBoard['8'] == "O" and theBoard['7'] == ' ': # across the top
+            place(7)
+            round += 1
+            turn += 1
+        elif theBoard['7'] == theBoard['9'] == "O" and theBoard['8'] == ' ': # across the top
+            place(8)
+            round += 1
+            turn += 1
+        elif theBoard['4'] == theBoard['5'] == "O" and theBoard['6'] == ' ': # across mid
+            place(6)
+            round += 1
+            turn += 1
+        elif theBoard['6'] == theBoard['5'] == "O" and theBoard['4'] == ' ': # across mid
+            place(4)
+            round += 1
+            turn += 1
+        elif theBoard['4'] == theBoard['6'] == "O" and theBoard['5'] == ' ': # across mid
+            place(5)
+            round += 1
+            turn += 1
+        elif theBoard['1'] == theBoard['2'] == "O" and theBoard['3'] == ' ': # across bottom
+            place(3)
+            round += 1
+            turn += 1
+        elif theBoard['3'] == theBoard['2'] == "O" and theBoard['1'] == ' ': # across bottom
+            place(1)
+            round += 1
+            turn += 1
+        elif theBoard['1'] == theBoard['3'] == "O" and theBoard['2'] == ' ': # across bottom
+            place(2)
+            round += 1
+            turn += 1
+        elif theBoard['7'] == theBoard['4'] == "O" and theBoard['1'] == ' ': # down left
+            place(1)
+            round += 1
+            turn += 1
+        elif theBoard['1'] == theBoard['4'] == "O" and theBoard['7'] == ' ': # down left
+            place(7)
+            round += 1
+            turn += 1
+        elif theBoard['7'] == theBoard['1'] == "O" and theBoard['4'] == ' ': # down left
+            place(4)
+            round += 1
+            turn += 1
+        elif theBoard['8'] == theBoard['5'] == "O" and theBoard['2'] == ' ': # down mid
+            place(2)
+            round += 1
+            turn += 1
+        elif theBoard['2'] == theBoard['5'] == "O" and theBoard['8'] == ' ': # down mid
+            place(8)
+            round += 1
+            turn += 1
+        elif theBoard['8'] == theBoard['2'] == "O" and theBoard['5'] == ' ': # down mid
+            place(5)
+            round += 1
+            turn += 1
+        elif theBoard['9'] == theBoard['6'] == "O" and theBoard['3'] == ' ': # down right
+            place(3)
+            round += 1
+            turn += 1
+        elif theBoard['3'] == theBoard['6'] == "O" and theBoard['9'] == ' ': # down right
+            place(9)
+            round += 1
+            turn += 1
+        elif theBoard['9'] == theBoard['3'] == "O" and theBoard['6'] == ' ': # down right
+            place(6)
+            round += 1
+            turn += 1
+        elif theBoard['9'] == theBoard['5'] == "O" and theBoard['1'] == ' ': # right diagonal
+            place(1)
+            round += 1
+            turn += 1
+        elif theBoard['9'] == theBoard['1'] == "O" and theBoard['5'] == ' ': # right diagonal
+            place(5)
+            round += 1
+            turn += 1
+        elif theBoard['1'] == theBoard['5'] == "O" and theBoard['9'] == ' ': # right diagonal
+            place(9)
+            round += 1
+            turn += 1
+        elif theBoard['7'] == theBoard['5'] == "O" and theBoard['3'] == ' ': # left diagonal
+            place(3)
+            round += 1
+            turn += 1
+        elif theBoard['3'] == theBoard['5'] == "O" and theBoard['7'] == ' ': # left diagonal
+            place(7)
+            round += 1
+            turn += 1
+        elif theBoard['7'] == theBoard['3'] == "O" and theBoard['5'] == ' ': # left diagonal
+            place(5)
+            round += 1
+            turn += 1
+
+        elif theBoard['7'] == theBoard['8'] == "X" and theBoard['9'] == ' ': # across the top
+            place(9)
+            round += 1
+            turn += 1
+        elif theBoard['9'] == theBoard['8'] == "X" and theBoard['7'] == ' ': # across the top
+            place(7)
+            round += 1
+            turn += 1
+        elif theBoard['7'] == theBoard['9'] == "X" and theBoard['8'] == ' ': # across the top
+            place(8)
+            round += 1
+            turn += 1
+        elif theBoard['4'] == theBoard['5'] == "X" and theBoard['6'] == ' ': # across mid
+            place(6)
+            round += 1
+            turn += 1
+        elif theBoard['6'] == theBoard['5'] == "X" and theBoard['4'] == ' ': # across mid
+            place(4)
+            round += 1
+            turn += 1
+        elif theBoard['4'] == theBoard['6'] == "X" and theBoard['5'] == ' ': # across mid
+            place(5)
+            round += 1
+            turn += 1
+        elif theBoard['1'] == theBoard['2'] == "X" and theBoard['3'] == ' ': # across bottom
+            place(3)
+            round += 1
+            turn += 1
+        elif theBoard['3'] == theBoard['2'] == "X" and theBoard['1'] == ' ': # across bottom
+            place(1)
+            round += 1
+            turn += 1
+        elif theBoard['1'] == theBoard['3'] == "X" and theBoard['2'] == ' ': # across bottom
+            place(2)
+            round += 1
+            turn += 1
+        elif theBoard['7'] == theBoard['4'] == "X" and theBoard['1'] == ' ': # down left
+            place(1)
+            round += 1
+            turn += 1
+        elif theBoard['1'] == theBoard['4'] == "X" and theBoard['7'] == ' ': # down left
+            place(7)
+            round += 1
+            turn += 1
+        elif theBoard['7'] == theBoard['1'] == "X" and theBoard['4'] == ' ': # down left
+            place(4)
+            round += 1
+            turn += 1
+        elif theBoard['8'] == theBoard['5'] == "X" and theBoard['2'] == ' ': # down mid
+            place(2)
+            round += 1
+            turn += 1
+        elif theBoard['2'] == theBoard['5'] == "X" and theBoard['8'] == ' ': # down mid
+            place(8)
+            round += 1
+            turn += 1
+        elif theBoard['8'] == theBoard['2'] == "X" and theBoard['5'] == ' ': # down mid
+            place(5)
+            round += 1
+            turn += 1
+        elif theBoard['9'] == theBoard['6'] == "X" and theBoard['3'] == ' ': # down right
+            place(3)
+            round += 1
+            turn += 1
+        elif theBoard['3'] == theBoard['6'] == "X" and theBoard['9'] == ' ': # down right
+            place(9)
+            round += 1
+            turn += 1
+        elif theBoard['9'] == theBoard['3'] == "X" and theBoard['6'] == ' ': # down right
+            place(6)
+            round += 1
+            turn += 1
+        elif theBoard['9'] == theBoard['5'] == "X" and theBoard['1'] == ' ': # right diagonal
+            place(1)
+            round += 1
+            turn += 1
+        elif theBoard['9'] == theBoard['1'] == "X" and theBoard['5'] == ' ': # right diagonal
+            place(5)
+            round += 1
+            turn += 1
+        elif theBoard['1'] == theBoard['5'] == "X" and theBoard['9'] == ' ': # right diagonal
+            place(9)
+            round += 1
+            turn += 1
+        elif theBoard['7'] == theBoard['5'] == "X" and theBoard['3'] == ' ': # left diagonal
+            place(3)
+            round += 1
+            turn += 1
+        elif theBoard['3'] == theBoard['5'] == "X" and theBoard['7'] == ' ': # left diagonal
+            place(7)
+            round += 1
+            turn += 1
+        elif theBoard['7'] == theBoard['3'] == "X" and theBoard['5'] == ' ': # left diagonal
+            place(5)
+            round += 1
+            turn += 1
+
+        else:
+            if round == 2 and theBoard['5'] == ' ':
+                place(5)
+                round += 1
+            else:
+                while plan != 10:
+                    plan = random.randint(1,9)
+                    if theBoard[str(plan)] == ' ':
+                        place(plan)
+                        plan = 10
+                        round += 1
+
+    checkWin()
+    if end == 1:
+        i = 6
+        break
+
+printBoard(theBoard)
+print("\nGame Over.\n")   
+if turn == 0:             
+    print(" ****  You  win. ****")
+elif turn == 2:
+    print(" ****  AI  wins. ****")
+    
+else:
+    print("0 wuns")
