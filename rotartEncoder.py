fc2c0e5f844d82aaf8167554e87bec3d13f39e8c
Avanhoo <avanhoo2498@charlottesvillecityschools.org>
2023-03-21 09:50:10 -0400

    Added working rotary encoder code

--- rotartEncoder.py
+++ rotartEncoder.py
@@ -1,6 +1,5 @@
 import board
-from analogio import AnalogIn, AnalogOut
-from digitalio import DigitalInOut, Direction
+from digitalio import DigitalInOut, Direction, Pull
 from lcd.lcd import LCD
 from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
 from time import sleep
@@ -8,45 +7,49 @@ import rotaryio
 
 i2c = board.I2C()
 lcd = LCD(I2CPCF8574Interface(i2c, 0x27), num_rows=2, num_cols=16)
-lcd.print("Please?")
 sleep(1)
-button = DigitalInOut(2)
-button.direction = 
-rot = rotaryio.IncrementalEncoder(3, 4)
-Rled = AnalogOut(board.A0) #Setup for the 3 lights
-Yled = AnalogOut(board.A1)
-Gled = AnalogOut(board.A2)
+button = DigitalInOut(board.D2)
+button.direction = Direction.INPUT
+button.pull = Pull.DOWN
+rot = rotaryio.IncrementalEncoder(board.D3, board.D4, divisor=2) # Make sure the divisor is 2 so that it registers every increment
+Rled = DigitalInOut(board.D8) #         Setup for the 3 lights
+Rled.direction = Direction.OUTPUT
+Yled = DigitalInOut(board.D9)
+Yled.direction = Direction.OUTPUT
+Gled = DigitalInOut(board.D10)
+Gled.direction = Direction.OUTPUT
 
 Trafc = {1: 1, 2: 0, 3: 0}
+tName = {1: "Red", 2: "Yellow", 3: "Green"}
 spin = 1
 lastSpin = 1
 speed = 1
+last_pos = 0
 
 lcd.print("Begin")
-sleep(1)
+Trafc[1] = True
 while True:
-    spin = rot.position
+    spin += rot.position - last_pos
+    last_pos = rot.position
     if spin > 3: # Keeps spin from 1-3
         spin = 1
     elif spin <1:
         spin = 3
 
-    if spin != speed: # Turns on the highlighted light
-        Trafc[spin] = .25
-
     if spin != lastSpin: # Updates the highlighted light
         print(spin)
         lcd.clear()
-        lcd.print(spin)
-        if lastSpin != speed: # Turns off the old highlighted light
-           Trafc[lastSpin] = 0
+        lcd.print("Change To:      "+ tName[spin]) # I use an array to avoid any if statement shenanigans
         lastSpin = spin
 
-    if button == True: # When the button is pressed it turns on the selected light and turns off the old one
-        speed = spin
-        Trafc[speed] = 0
-        Trafc[spin] = 1
+    if not button.value: # When the button is pressed it turns on the selected light and turns off the old one
+        print("Press")
+        Trafc[speed] = False # Turns off old light
+        speed = spin # Updates speed value
+        Trafc[spin] = True # Turns on new light
     
     Rled.value = Trafc[1]
     Yled.value = Trafc[2]
     Gled.value = Trafc[3]
+    while not button.value: # Stops you from accidentally selecting another color when the button is pressed
+        sleep(.1)
