55218509b72d90cb705e23e6d5a490e41de9ebed
Avanhoo <avanhoo2498@charlottesvillecityschools.org>
2023-03-27 09:56:45 -0400

    a

--- tictactod.py
+++ tictactod.py
@@ -4,6 +4,7 @@ import random
 import pwmio
 import servo
 from analogio import AnalogIn
+from digitalio import DigitalInOut, Direction
 
 
 turn = 0 # whose turn it is, 0 is player, 1 is AI
@@ -12,11 +13,13 @@ end = 0 # if the game has ended
 plan = 0 # where the AI is planning to move
 extend = 0 # how far out the arm is
 twist = 0 # angle of turn of the arm
-offset = 6
+offset = 3
 armProg = 96
 colorBase = 0
-scanN = 1
+Nscan = 1
 
+button = DigitalInOut(board.D8)
+button.direction = Direction.INPUT
 pwm = pwmio.PWMOut(board.A1, duty_cycle=2 ** 15, frequency=50)
 pwm2 = pwmio.PWMOut(board.A5, duty_cycle=2 ** 15, frequency=50)
 uppy = pwmio.PWMOut(board.D7, duty_cycle=0, frequency=50, variable_frequency=True) # Sets up a PWM output for the magnet servo as there are no more A timers
@@ -24,7 +27,7 @@ uppy = pwmio.PWMOut(board.D7, duty_cycle=0, frequency=50, variable_frequency=Tru
 color = AnalogIn(board.A2)
 
 
-theBoard = {'7': ' ' , '8': ' ' , '9': ' ' , # The visual board, keeps track of placed X's and O's
+theBoard = {'7': ' ' , '8': ' ' , '9': ' ' , # The visual board, keeps track of placed O's and X's
             '4': ' ' , '5': ' ' , '6': ' ' ,
             '1': ' ' , '2': ' ' , '3': ' ' }
 
@@ -126,7 +129,7 @@ def place(spot): # Code to move the arm to a specified place on the board
     print("Pickup")
 
     
-    theBoard[str(spot)] = "O"
+    theBoard[str(spot)] = "X"
     armProg = spinny.angle
     while spinny.angle != angleBoard[str(spot)] + offset: # code to move arm turn smoothly
         if abs(armProg - (angleBoard[str(spot)] + offset)) < 2:
@@ -183,29 +186,40 @@ def place(spot): # Code to move the arm to a specified place on the board
         sleep(.0001)
 
 def scan():
-    scanN = 1
-    while scanN < 10:
-        if theBoard[str(scanN)] != 'X' and theBoard[str(scanN)] != 'O':
-            arm.angle = distBoard[str(scanN)]
-            sleep(.25)
-            spinny.angle = angleBoard[str(scanN)]
-            sleep(.4)
-            if (color.value - colorBase) > 3000:
-                theBoard[str(scanN)] = 'X'
-                break
-            print(scanN)
+    Nscan = 1
+    print ("B: "+ str(colorBase))
+    while Nscan < 10:
+        if theBoard[str(Nscan)] != 'O' and theBoard[str(Nscan)] != 'X':
+            arm.angle = distBoard[str(Nscan)]
+            #sleep(.3)
+            spinny.angle = angleBoard[str(Nscan)]
+            sleep(.3)
+            for e in range (1,7):                         # PROBLEM FIX PROBLEM
+                if (abs(color.value - colorBase)/((color.value + colorBase)/2))*100 > 22.5:
+                    print(color.value)
+                    print("O at " + str(Nscan))
+                    theBoard[str(Nscan)] = 'O'
+                    Nscan = 20
+                    break
+                print(color.value)
+                e = 1
+                sleep(.1)
+            print(Nscan)
 
-
-        if scanN < 3:
-            scanN += 1
-        elif scanN == 3:
-            scanN = 6
-        elif scanN > 4 and scanN < 7:
-            scanN -= 1
-        elif scanN == 4:
-            scanN = 7
-        elif scanN > 6:
-            scanN += 1
+        if Nscan == 20:
+            pass
+        elif Nscan < 3:
+            Nscan += 1
+        elif Nscan == 3:
+            Nscan = 6
+        elif Nscan > 4 and Nscan < 7:
+            Nscan -= 1
+        elif Nscan == 4:
+            Nscan = 7
+        elif Nscan > 6:
+            Nscan += 1
+    if (Nscan > 9) and (Nscan != 20):
+        scan()
 
 
 arm.angle = distBoard['5']
@@ -216,7 +230,6 @@ for i in range(10):
     sleep(.05)
 colorBase /= 10
 print(colorBase)
-scan()
 print("Move with the numpad.")
 sleep(1)
 
@@ -231,6 +244,7 @@ for i in range(5): # The main tic tac toe loop
     print("--------------------------------")
 
 #Start
+    '''
     print("Where Would you like to move?")
     move = input("")
 
@@ -248,7 +262,7 @@ for i in range(5): # The main tic tac toe loop
             i -=1
             continue
         elif int(move) < 10 and  int(move) > 0 and theBoard[move] == " ":
-            theBoard[str(move)] = "X"
+            theBoard[str(move)] = "O"
             round += 1
         else:
             print("Invalid move, try again")
@@ -257,8 +271,12 @@ for i in range(5): # The main tic tac toe loop
     except:
         print("Invalid move, try again")
         continue
+    '''
 # End Removal
-
+    while button.value:
+        sleep(.5)
+    print("Pressed!")
+    scan()
     checkWin()
     if end == 1:
         i = 6
@@ -271,104 +289,7 @@ for i in range(5): # The main tic tac toe loop
     sleep(1)
     turn = 1
     if True:
-        if theBoard['7'] == theBoard['8'] == "O" and theBoard['9'] == ' ': # across the top
-            place(9)
-            round += 1
-            turn += 1
-        elif theBoard['9'] == theBoard['8'] == "O" and theBoard['7'] == ' ': # across the top
-            place(7)
-            round += 1
-            turn += 1
-        elif theBoard['7'] == theBoard['9'] == "O" and theBoard['8'] == ' ': # across the top
-            place(8)
-            round += 1
-            turn += 1
-        elif theBoard['4'] == theBoard['5'] == "O" and theBoard['6'] == ' ': # across mid
-            place(6)
-            round += 1
-            turn += 1
-        elif theBoard['6'] == theBoard['5'] == "O" and theBoard['4'] == ' ': # across mid
-            place(4)
-            round += 1
-            turn += 1
-        elif theBoard['4'] == theBoard['6'] == "O" and theBoard['5'] == ' ': # across mid
-            place(5)
-            round += 1
-            turn += 1
-        elif theBoard['1'] == theBoard['2'] == "O" and theBoard['3'] == ' ': # across bottom
-            place(3)
-            round += 1
-            turn += 1
-        elif theBoard['3'] == theBoard['2'] == "O" and theBoard['1'] == ' ': # across bottom
-            place(1)
-            round += 1
-            turn += 1
-        elif theBoard['1'] == theBoard['3'] == "O" and theBoard['2'] == ' ': # across bottom
-            place(2)
-            round += 1
-            turn += 1
-        elif theBoard['7'] == theBoard['4'] == "O" and theBoard['1'] == ' ': # down left
-            place(1)
-            round += 1
-            turn += 1
-        elif theBoard['1'] == theBoard['4'] == "O" and theBoard['7'] == ' ': # down left
-            place(7)
-            round += 1
-            turn += 1
-        elif theBoard['7'] == theBoard['1'] == "O" and theBoard['4'] == ' ': # down left
-            place(4)
-            round += 1
-            turn += 1
-        elif theBoard['8'] == theBoard['5'] == "O" and theBoard['2'] == ' ': # down mid
-            place(2)
-            round += 1
-            turn += 1
-        elif theBoard['2'] == theBoard['5'] == "O" and theBoard['8'] == ' ': # down mid
-            place(8)
-            round += 1
-            turn += 1
-        elif theBoard['8'] == theBoard['2'] == "O" and theBoard['5'] == ' ': # down mid
-            place(5)
-            round += 1
-            turn += 1
-        elif theBoard['9'] == theBoard['6'] == "O" and theBoard['3'] == ' ': # down right
-            place(3)
-            round += 1
-            turn += 1
-        elif theBoard['3'] == theBoard['6'] == "O" and theBoard['9'] == ' ': # down right
-            place(9)
-            round += 1
-            turn += 1
-        elif theBoard['9'] == theBoard['3'] == "O" and theBoard['6'] == ' ': # down right
-            place(6)
-            round += 1
-            turn += 1
-        elif theBoard['9'] == theBoard['5'] == "O" and theBoard['1'] == ' ': # right diagonal
-            place(1)
-            round += 1
-            turn += 1
-        elif theBoard['9'] == theBoard['1'] == "O" and theBoard['5'] == ' ': # right diagonal
-            place(5)
-            round += 1
-            turn += 1
-        elif theBoard['1'] == theBoard['5'] == "O" and theBoard['9'] == ' ': # right diagonal
-            place(9)
-            round += 1
-            turn += 1
-        elif theBoard['7'] == theBoard['5'] == "O" and theBoard['3'] == ' ': # left diagonal
-            place(3)
-            round += 1
-            turn += 1
-        elif theBoard['3'] == theBoard['5'] == "O" and theBoard['7'] == ' ': # left diagonal
-            place(7)
-            round += 1
-            turn += 1
-        elif theBoard['7'] == theBoard['3'] == "O" and theBoard['5'] == ' ': # left diagonal
-            place(5)
-            round += 1
-            turn += 1
-
-        elif theBoard['7'] == theBoard['8'] == "X" and theBoard['9'] == ' ': # across the top
+        if theBoard['7'] == theBoard['8'] == "X" and theBoard['9'] == ' ': # across the top
             place(9)
             round += 1
             turn += 1
@@ -461,6 +382,103 @@ for i in range(5): # The main tic tac toe loop
             round += 1
             turn += 1
         elif theBoard['7'] == theBoard['3'] == "X" and theBoard['5'] == ' ': # left diagonal
+            place(5)
+            round += 1
+            turn += 1
+
+        elif theBoard['7'] == theBoard['8'] == "O" and theBoard['9'] == ' ': # across the top
+            place(9)
+            round += 1
+            turn += 1
+        elif theBoard['9'] == theBoard['8'] == "O" and theBoard['7'] == ' ': # across the top
+            place(7)
+            round += 1
+            turn += 1
+        elif theBoard['7'] == theBoard['9'] == "O" and theBoard['8'] == ' ': # across the top
+            place(8)
+            round += 1
+            turn += 1
+        elif theBoard['4'] == theBoard['5'] == "O" and theBoard['6'] == ' ': # across mid
+            place(6)
+            round += 1
+            turn += 1
+        elif theBoard['6'] == theBoard['5'] == "O" and theBoard['4'] == ' ': # across mid
+            place(4)
+            round += 1
+            turn += 1
+        elif theBoard['4'] == theBoard['6'] == "O" and theBoard['5'] == ' ': # across mid
+            place(5)
+            round += 1
+            turn += 1
+        elif theBoard['1'] == theBoard['2'] == "O" and theBoard['3'] == ' ': # across bottom
+            place(3)
+            round += 1
+            turn += 1
+        elif theBoard['3'] == theBoard['2'] == "O" and theBoard['1'] == ' ': # across bottom
+            place(1)
+            round += 1
+            turn += 1
+        elif theBoard['1'] == theBoard['3'] == "O" and theBoard['2'] == ' ': # across bottom
+            place(2)
+            round += 1
+            turn += 1
+        elif theBoard['7'] == theBoard['4'] == "O" and theBoard['1'] == ' ': # down left
+            place(1)
+            round += 1
+            turn += 1
+        elif theBoard['1'] == theBoard['4'] == "O" and theBoard['7'] == ' ': # down left
+            place(7)
+            round += 1
+            turn += 1
+        elif theBoard['7'] == theBoard['1'] == "O" and theBoard['4'] == ' ': # down left
+            place(4)
+            round += 1
+            turn += 1
+        elif theBoard['8'] == theBoard['5'] == "O" and theBoard['2'] == ' ': # down mid
+            place(2)
+            round += 1
+            turn += 1
+        elif theBoard['2'] == theBoard['5'] == "O" and theBoard['8'] == ' ': # down mid
+            place(8)
+            round += 1
+            turn += 1
+        elif theBoard['8'] == theBoard['2'] == "O" and theBoard['5'] == ' ': # down mid
+            place(5)
+            round += 1
+            turn += 1
+        elif theBoard['9'] == theBoard['6'] == "O" and theBoard['3'] == ' ': # down right
+            place(3)
+            round += 1
+            turn += 1
+        elif theBoard['3'] == theBoard['6'] == "O" and theBoard['9'] == ' ': # down right
+            place(9)
+            round += 1
+            turn += 1
+        elif theBoard['9'] == theBoard['3'] == "O" and theBoard['6'] == ' ': # down right
+            place(6)
+            round += 1
+            turn += 1
+        elif theBoard['9'] == theBoard['5'] == "O" and theBoard['1'] == ' ': # right diagonal
+            place(1)
+            round += 1
+            turn += 1
+        elif theBoard['9'] == theBoard['1'] == "O" and theBoard['5'] == ' ': # right diagonal
+            place(5)
+            round += 1
+            turn += 1
+        elif theBoard['1'] == theBoard['5'] == "O" and theBoard['9'] == ' ': # right diagonal
+            place(9)
+            round += 1
+            turn += 1
+        elif theBoard['7'] == theBoard['5'] == "O" and theBoard['3'] == ' ': # left diagonal
+            place(3)
+            round += 1
+            turn += 1
+        elif theBoard['3'] == theBoard['5'] == "O" and theBoard['7'] == ' ': # left diagonal
+            place(7)
+            round += 1
+            turn += 1
+        elif theBoard['7'] == theBoard['3'] == "O" and theBoard['5'] == ' ': # left diagonal
             place(5)
             round += 1
             turn += 1
