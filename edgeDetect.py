ff83e8aef3edb6dde72a2a011278cbe37361c5a4
Avanhoo <avanhoo2498@charlottesvillecityschools.org>
2023-06-02 09:57:26 -0400

    gagsgsdagdsfa

--- edgeDetect.py
+++ edgeDetect.py
@@ -1,37 +1,28 @@
 import cv2
 import numpy as np
- 
+
 # Read the original image
-img = cv2.imread('test.jpg') 
-# Display original image
-cv2.imshow('Original', img)
-cv2.waitKey(0)
- 
+img = cv2.imread('test.jpg')
+
 # Convert to graycsale
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# Blur the image for better edge detection
-img_blur = cv2.GaussianBlur(img_gray, (5,5), 0) 
- 
-# Sobel Edge Detection
 
-sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5) # Sobel Edge Detection on the X axis
-sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5) # Sobel Edge Detection on the Y axis
-sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) # Combined X and Y Sobel Edge Detection
-# Display Sobel Edge Detection Images
-#cv2.imshow('Sobel X', sobelx)
-#cv2.waitKey(0)
-#cv2.imshow('Sobel Y', sobely)
-#cv2.waitKey(0)
-#cv2.imshow('Sobel X Y using Sobel() function', sobelxy)
-#cv2.waitKey(0)
- 
-# Canny Edge Detection
-edges = cv2.Canny(image=img_blur, threshold1=75, threshold2=10) # Canny Edge Detection
-# Display Canny Edge Detection Image
-cv2.imshow('Canny Edge Detection', edges)
+# Display greyscale image
+cv2.imshow('Grey', img_gray)
 cv2.waitKey(0)
+ 
+# Blur the image for better edge detection
+img_blur = cv2.GaussianBlur(img_gray, (7,7), 0) 
+ 
+ 
+edges = cv2.Canny(image=img_blur, threshold1=65, threshold2=10) # Canny Edge Detection
+cv2.imshow('Canny Edge Detection', edges) # Display Canny Edge Detection Image
 
-merge = cv2.addWeighted(src1, alpha, src2, beta, 0.0)
-cv2.imshow('Merged', vis)
+cv2.waitKey(0)
+cv2.imwrite('edge.jpg', edges)
+
+
+merge = cv2.addWeighted(img_gray, .75, edges, .25, 0.0)
+cv2.imshow('Merged', merge)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
